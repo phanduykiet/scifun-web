7113262a6388ab5dd184ea517344205649bba7ab
gialong162004 <gialongpbc16@gmail.com>
2025-10-29 22:56:49 +0700

    - UI Leaderboard

--- scifun-ui/src/App.tsx
+++ scifun-ui/src/App.tsx
@@ -14,6 +14,7 @@ import SubjectPage from "./pages/Subject";
 import TopicPage from "./pages/Topic";
 import StatisticsPage from "./pages/StatisticsPage";
 import SavedQuizsPage from "./pages/SavedQuizsPage";
+import Leaderboard from "./pages/Leaderboard";
 
 export default function App() {
   return (
@@ -33,6 +34,7 @@ export default function App() {
         <Route path="/topic/:topicId" element={<TopicPage />} />
         <Route path="/statistic" element={<StatisticsPage />} />
         <Route path="/save-quiz" element={<SavedQuizsPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
   );
 }
