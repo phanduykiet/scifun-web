44b3b8dc0993f29b53b8729bd0c2126bbcf77b25
phankiet <phanduykiet1@gmail.com>
2025-10-29 15:29:28 +0700

    update

--- scifun-api/src/services/quizService.ts
+++ scifun-api/src/services/quizService.ts
@@ -1,5 +1,6 @@
 import Quiz, { IQuiz } from "../models/Quiz";
 import { esClient } from "../config/elasticSearch";
+import { notifyNewQuiz } from "./notificationService";
 
 const QUIZ_INDEX = "quiz";
 
@@ -15,6 +16,12 @@ export const createQuizSv = async (data: Partial<IQuiz>) => {
   });
   // Sync lên ES
   await syncOneQuizToES(quiz._id.toString());
+
+  await notifyNewQuiz(
+    quiz._id.toString(),
+    quiz.title,
+    data.topic!.toString()
+  );
 
   return quiz;
 };
